1cf42e8f90a31788ccffc5b2ca1959fa0004d030
Mario Camara <mariocamaraf@gmail.com>
2024-09-15 17:49:40 +0200

    useMemo for apolloWrapper

--- lib/apollo-wrapper.tsx
+++ lib/apollo-wrapper.tsx
@@ -1,12 +1,17 @@
 'use client'
 
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { useMemo } from 'react'
 
 export function ApolloWrapper({ children }: React.PropsWithChildren) {
-  const client = new ApolloClient({
-    uri: '/api/graphql',
-    cache: new InMemoryCache(),
-  })
+  const client = useMemo(
+    () =>
+      new ApolloClient({
+        uri: '/api/graphql',
+        cache: new InMemoryCache(),
+      }),
+    []
+  )
 
   return <ApolloProvider client={client}>{children}</ApolloProvider>
 }
